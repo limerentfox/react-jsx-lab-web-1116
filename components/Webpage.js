8521f8dc2ac8582d1233517f3dd2e945b938c69e
limerentfox <christi@limerentfox.com>
2017-01-31 14:36:27 -0500

    Done.

---
+++ components/Webpage.js
@@ -0,0 +1,18 @@
+const React = require('react')
+const FillerText = require('../components/FillerText')
+
+
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>The world's coolest webpage</h1>
+      <FillerText />
+      <FillerText />
+      </div>
+    )
+  }
+}
+
+
+module.exports = Webpage
